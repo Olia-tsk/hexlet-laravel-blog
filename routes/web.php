e469643e30db9b6ecef2024b63bc2ba920823597
Ольга Курбатова <kon@2i.tusur.ru>
2025-04-18 15:06:19 +0700

    add about roure & view

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,7 @@ Route::get('/', function () {
 Route::get('posts', function () {
     // logic
 })->name('posts.store');
+
+Route::get('about', function () {
+    return view('about');
+})->name('about');
