a44452c5b805b70b4fb0adef88f95423e40a5851
Ольга Курбатова <kon@2i.tusur.ru>
2025-04-25 14:46:58 +0700

    add Page Controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,7 +11,4 @@ Route::get('posts', function () {
     // logic
 })->name('posts.store');
 
-Route::get('about', function () {
-    $tags = ['education', 'programming', 'php', 'oop'];
-    return view('about', ['tags' => $tags]);
-})->name('about');
+Route::get('about', [PageController::class, 'about'])->name('about');
