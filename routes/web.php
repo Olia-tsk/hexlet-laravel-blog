3e3dc35fd05539fd5c5e0f9881cf84340508d19f
Ольга Курбатова <kon@2i.tusur.ru>
2025-04-28 12:43:34 +0700

    add GET /articles/{id} route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ Route::get('about', [PageController::class, 'about'])->name('about');
 
 Route::get('articles', [ArticleController::class, 'index'])->name('articles.index');
 
+Route::get('articles/{id}', [ArticleController::class, 'show'])->name('articles.show');
