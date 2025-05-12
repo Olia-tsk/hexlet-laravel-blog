e05e01e9896fac496ee3a54ab25c7b49398577ec
Ольга Курбатова <kon@2i.tusur.ru>
2025-05-12 18:14:57 +0700

    add bootstrap

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -3,12 +3,8 @@
 @section('messages')
     @if (Session::has('flash_message'))
         <div class="container">
-            <div class="row">
-                <div class="col-md-10">
-                    <div class="alert alert-success">
-                        {{ Session::get('flash_message') }}
-                    </div>
-                </div>
+            <div class="alert alert-success" role="alert">
+                {{ Session::get('flash_message') }}
             </div>
         </div>
     @endif
@@ -18,9 +14,22 @@
 
 @section('content')
     @foreach ($articles as $article)
+        <div class="card mb-4">
+            <div class="card-body">
+                <h5 class="card-title">
+                    <a href="{{ route('articles.show', ['id' => $article->id]) }}">{{ $article->name }}</a>
+                </h5>
+                <p class="card-text">
+                <div>{{ Str::limit($article->body, 200) }}</div>
+                </p>
+                <a href="{{ route('articles.edit', $article) }} " class="btn btn-primary">
+                    Edit article
+                </a>
                 <a href="{{ route('articles.destroy', $article) }}" data-confirm="SURE?" data-method="delete" rel="nofollow"
                     class="btn btn-danger">
                     Delete article
                 </a>
+            </div>
+        </div>
     @endforeach
 @endsection
