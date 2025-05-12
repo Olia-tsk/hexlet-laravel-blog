be371503a4ac77890ed04af14dfe3971e161b168
Ольга Курбатова <kon@2i.tusur.ru>
2025-05-12 18:13:05 +0700

    add delete link

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -18,12 +18,9 @@
 
 @section('content')
     @foreach ($articles as $article)
-        <h2>
-            <a href="{{ route('articles.show', ['id' => $article->id]) }}">{{ $article->name }}</a>
-        </h2>
-
-        <div>{{ Str::limit($article->body, 200) }}</div>
-
-        <a href="{{ route('articles.edit', ['id' => $article->id]) }}">Edit article</a>
+                <a href="{{ route('articles.destroy', $article) }}" data-confirm="SURE?" data-method="delete" rel="nofollow"
+                    class="btn btn-danger">
+                    Delete article
+                </a>
     @endforeach
 @endsection
